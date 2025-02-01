4f520084b6d05a59660189e8b5d52af58ffc3ca1
Mohammed Arshad <arshadff02@gmail.com>
2025-02-01 23:39:48 +0530

    changes

--- Products/src/components/AdvanceProducts.tsx
+++ Products/src/components/AdvanceProducts.tsx
@@ -23,7 +23,7 @@ function Products(prop: propProducts) {
                     <div className="col" key={product.id}>
                         <div className="card h-100">
                             <img src={product.image} className="card-img-top" alt={product.title} style={{ objectFit: 'cover', height: '200px' }} />
-                            <div className="card-body">
+                            <div className="card-body" key={product.id}>
                                 <h5 className="card-title">{product.title}</h5>
                                 <p className="card-text">{product.description}</p>
                                 <p className="card-text"><strong>Category:</strong> {product.category}</p>
